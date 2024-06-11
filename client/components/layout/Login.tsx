402180f2dcaf04b25202afcd3c09ee8c030bdf84
Jack Wilson <jxwilx@gmail.com>
2024-06-12 10:06:04 +1200

    signin done, button altered

---
+++ client/components/layout/Login.tsx
@@ -0,0 +1,35 @@
+import { IfAuth, IfNotAuth } from '../Auth'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Button } from '@/components/Button.tsx'
+
+function Login() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  const handleLogin = () => {
+    console.log('logging in')
+    return loginWithRedirect()
+  }
+
+  const handleLogout = () => {
+    console.log('logging out')
+    return logout()
+  }
+
+  return (
+    <>
+      <IfAuth>
+        {user && <p>Welcome {user?.nickname}</p>}
+        <Button type="none" onClick={handleLogout}>
+          Log out
+        </Button>
+      </IfAuth>
+      <IfNotAuth>
+        <Button type="none" onClick={handleLogin}>
+          Log in
+        </Button>
+      </IfNotAuth>
+    </>
+  )
+}
+
+export default Login
