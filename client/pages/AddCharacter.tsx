f963fb460b2b4e1d15972ca753cffd84b797e29a
Luke Pryor <14864031+mizticNZ@users.noreply.github.com>
2024-06-12 11:08:47 +1200

    add-character Form layout done

--- client/pages/AddCharacter.tsx
+++ client/pages/AddCharacter.tsx
@@ -1,13 +1,57 @@
+import { Button } from '@/components/Button'
 import PageTitle from '@/components/PageTitle'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Form } from 'react-router-dom'
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
 
-function NotFound() {
+function AddCharacter() {
   return (
     <>
       <>
         <PageTitle title="Create Character" />
+        <div>
+          <Form>
+            <label>Name</label>
+            <div style={{ marginTop: '5px' }}>
+              <input
+                type="name"
+                placeholder="Example"
+                style={{
+                  padding: '5px',
+                  width: '50%',
+                  border: '1px solid white',
+                }}
+              ></input>
+            </div>
+            <div style={{ marginTop: '5px' }}>
+              <label style={{ display: 'block', marginBottom: '5px' }}>
+                Bio
+              </label>
+              <textarea
+                style={{
+                  width: '100%',
+                  padding: '5px',
+                  height: '150px',
+                  border: '1px solid white',
+                  boxSizing: 'border-box',
+                  color: 'black',
+                }}
+              ></textarea>
+            </div>
+            <div style={{ marginTop: '10px' }}>
+              <Button type="green">
+                <FontAwesomeIcon icon={faUpload} />
+              </Button>
+            </div>
+            <div style={{ marginTop: '15px' }}>
+              <Button type="green">Add</Button>
+              <Button type="green">Clear</Button>
+            </div>
+          </Form>
+        </div>
       </>
     </>
   )
 }
 
-export default NotFound
+export default AddCharacter
