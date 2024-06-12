57f22d7f63a21af84c416545bead3dbd042ab10f
Nathan Emmerson <100540895+Nathan-Emmerson@users.noreply.github.com>
2024-06-13 11:46:46 +1200

    added testing

--- client/pages/__test__/Character.test.tsx
+++ client/pages/__test__/Character.test.tsx
@@ -2,7 +2,8 @@ import { describe, it, expect } from 'vitest'
 import { JSDOM } from 'jsdom'
 import server from '../../../server/server.ts'
 import request from 'supertest'
-import { within } from '@testing-library/jest-dom'
+import { useCharacterById } from '../hooks/use-character-by-id'
+import { within } from '@testing-library/dom'
 
 const render = (response) => {
   const { document } = new JSDOM(response.text).window
@@ -12,26 +13,27 @@ export default render
 
 describe('Character front-end tests', () => {
   it('contains the character data', () => {
+    const characterId = 1
+    const { data: character } = useCharacterById(characterId)
     return request(server)
-    .get('/api/v1/characters/id')
-    .then((response)=> {
-      const screen = render(response)
-      const name = screen.getByText(character.name)
-      const id = screen.getByText(character.id)
-      const managerId = screen.getByText(character.managerId)
-      const bio = screen.getByText(character.bio)
-      const evilPoints = screen.getByText(character.evilPoints)
-      const goodPoints = screen.getByText(character.goodPoints)
-      const imgUrl = screen.getByText(character.imgUrl)
+      .get('/api/v1/characters/id')
+      .then((response) => {
+        const screen = render(response)
+        const name = screen.getByText(character.name)
+        const id = screen.getByText(character.id)
+        const managerId = screen.getByText(character.managerId)
+        const bio = screen.getByText(character.bio)
+        const evilPoints = screen.getByText(character.evilPoints)
+        const goodPoints = screen.getByText(character.goodPoints)
+        const imgUrl = screen.getByText(character.imgUrl)
 
-      expect(name).toBeInTheDocument()
-      expect(id).toBeInTheDocument()
-      expect(managerId).toBeInTheDocument()
-      expect(bio).toBeInTheDocument()
-      expect(evilPoints).toBeInTheDocument()
-      expect(goodPoints).toBeInTheDocument()
-      expect(imgUrl).toBeInTheDocument()
-      
-    })
+        expect(name).toBeInTheDocument()
+        expect(id).toBeInTheDocument()
+        expect(managerId).toBeInTheDocument()
+        expect(bio).toBeInTheDocument()
+        expect(evilPoints).toBeInTheDocument()
+        expect(goodPoints).toBeInTheDocument()
+        expect(imgUrl).toBeInTheDocument()
+      })
   })
 })
