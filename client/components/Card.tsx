4fa940201eb59f090aabcab02d0837b6556bb6fd
Finlay Neal <163089734+Finlay-Neal-05@users.noreply.github.com>
2024-06-13 16:24:20 +1200

    complete Game.tsx and Card.tsx styling. TODO: Home.tsx styling and Link components and useQuery testing

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -11,22 +11,22 @@ interface Props {
 export default function Card(props: Props) {
   return (
     <button
-      className={`h-[40em] w-[25em] rounded-[24px] shadow-md hover:shadow-xl my-auto hover:animate-wiggle`}
+      className={`relative h-[40em] w-[25em] rounded-[24px] shadow-md hover:shadow-xl my-auto hover:animate-wiggle`}
       onClick={props.onClick}
     >
-      <div className="relative rounded-t-[20px] h-2/3 w-full overflow-hidden">
+      <div className="relative rounded-[20px] h-full w-full overflow-hidden">
         <img
           className="object-cover h-full w-full"
           src={props.character.imgUrl}
           alt={props.character.name}
         />
-        <p className="absolute top-0 left-0 text-3xl h-auto w-full z-20 p-4 rounded-t-[20px] bg-gradient-to-b from-black to-none ">
+        <p className="absolute top-0 left-0 text-3xl h-auto w-full z-20 p-4 rounded-t-[20px bg-gradient-to-b from-black to-transparent]">
           {props.character.name}
         </p>
       </div>
-      <div className="relative h-1/3 z-20 text-black bg-slate-200 rounded-b-[20px] border-box border-4 border-t-0 p-4 font-light text-lg border-box">
+      <div className="absolute bottom-0 left-0 h-1/3 z-20 backdrop-blur-md bg-gradient-to-t from-black to-transparent rounded-b-[20px] border-box p-4 font-light text-lg border-box border-t-2 border-slate-200">
         <p className="pt-2">{props.character.bio}</p>
-        <div className="absolute bottom-0 left-0 flex justify-between w-full p-4 text-2xl">
+        <div className="absolute bottom-0 left-0 flex justify-between w-full p-4 text-2xl  rounded-b-[20px] ">
           <p className="text-green font-semibold">
             {props.character.goodPoints}
           </p>
