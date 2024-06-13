ab59898ce23f4ad5ce7a44edd210e4403c13ab2b
Finlay Neal <163089734+Finlay-Neal-05@users.noreply.github.com>
2024-06-13 15:39:27 +1200

    update CSS styling of card and add animations

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,16 +1,20 @@
 import { Character } from '@models/character'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmarkCircle } from '@fortawesome/free-regular-svg-icons'
 
 interface Props {
   key: number
-  character: Character,
+  character: Character
   onClick: () => void
 }
 
 export default function Card(props: Props) {
-
   return (
-    <button className="h-[40em] w-[25em] rounded-[24px] shadow-md hover:shadow-xl hover:scale-105 duration-75 my-auto" onClick={props.onClick}>
-      <div className="relative rounded-t-[20px] h-1/2 w-full overflow-hidden">
+    <button
+      className={`h-[40em] w-[25em] rounded-[24px] shadow-md hover:shadow-xl my-auto hover:animate-wiggle`}
+      onClick={props.onClick}
+    >
+      <div className="relative rounded-t-[20px] h-2/3 w-full overflow-hidden">
         <img
           className="object-cover h-full w-full"
           src={props.character.imgUrl}
@@ -20,11 +24,19 @@ export default function Card(props: Props) {
           {props.character.name}
         </p>
       </div>
-      <div className="h-1/2 z-20 text-black bg-green rounded-b-[20px] border-box border-4 border-t-0 p-4">
-        <p className="text-lg border-b-2 pb-2">Bio:</p>
+      <div className="relative h-1/3 z-20 text-black bg-slate-200 rounded-b-[20px] border-box border-4 border-t-0 p-4 font-light text-lg border-box">
         <p className="pt-2">{props.character.bio}</p>
-        <p>{props.character.goodPoints}</p>
-        <p>{props.character.evilPoints}</p>
+        <div className="absolute bottom-0 left-0 flex justify-between w-full p-4 text-2xl">
+          <p className="text-green font-semibold">
+            {props.character.goodPoints}
+          </p>
+          <FontAwesomeIcon
+            icon={faXmarkCircle}
+            className="mr-1 hover:scale-125 duration-75"
+            style={{ color: '#e66533' }}
+          />
+          <p className="text-red font-semibold">{props.character.evilPoints}</p>
+        </div>
       </div>
     </button>
   )
